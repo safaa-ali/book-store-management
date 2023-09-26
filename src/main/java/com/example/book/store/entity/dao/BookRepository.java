5def415ac10d2f8ccdfabae4b530aad05dd15da3
safaa-ali <79087972+safaa-ali@users.noreply.github.com>
2023-09-26 21:45:57 +0300

    Update BookRepository.java

--- src/main/java/com/example/book/store/entity/dao/BookRepository.java
+++ src/main/java/com/example/book/store/entity/dao/BookRepository.java
@@ -5,9 +5,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.example.book.store.entity.Book;
-
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 	@Query(value = "Select * from book b where " + "b.category=?", nativeQuery = true)
